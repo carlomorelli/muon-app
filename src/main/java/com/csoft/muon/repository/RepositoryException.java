86bc89231ed3399e21dbf029e5baf27ce27aff1c
Carlo Morelli <carlomorelli@gmail.com>
2017-05-03 19:49:21 +0200

    Repository implementation

--- src/main/java/com/csoft/muon/repository/RepositoryException.java
+++ src/main/java/com/csoft/muon/repository/RepositoryException.java
@@ -4,7 +4,15 @@ public class RepositoryException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
+    public RepositoryException() {
+        super();
+    }
+    
     public RepositoryException(String string) {
         super(string);
+    }
+    
+    public RepositoryException(String string, Throwable throwable) {
+        super(string, throwable);
     }
 }
--- src/main/java/com/csoft/muon/repository/RepositoryImpl.java
+++ src/main/java/com/csoft/muon/repository/RepositoryImpl.java
@@ -10,39 +10,57 @@ import com.csoft.muon.domain.Item;
 public class RepositoryImpl implements Repository {
 
     private Dao dao;
+    private Map<Integer, Item> registry;
+    
+    public RepositoryImpl() {
+        // initialization: fetch all existing database content, and put into registry
+        registry = dao.fetchAllItems()
+                .stream()
+                .collect(Collectors.toMap(item -> item.getIndex(), item -> item));
+    }
     
     @Override
     public Item get(int index) throws RepositoryException {
-        return dao.fetchAllItems()
-                .stream()
-                .filter(item -> item.getIndex() == index)
-                .findFirst()
-                .orElseThrow(() -> new RepositoryException("Unable to find item with given index " + index));
+        if (!registry.containsKey(index)) {
+            throw new RepositoryException("Unable to get item: given index is not used");
+        }
+        return registry.get(index);
     }
 
     @Override
     public Map<Integer, Item> getAll() {
-        return dao.fetchAllItems()
-                .stream()
-                .collect(Collectors.toMap(item -> item.getIndex(), item -> item));
+        return registry;
     }
 
     @Override
     public void append(Item item) throws RepositoryException {
+        if (registry.containsKey(item.getIndex())) {
+            throw new RepositoryException("Unable to insert item: given index is already used.");
+        }
         try {
             dao.insertItem(item);
         } catch (DaoException e) {
-            throw new RepositoryException("Unable to insert item: forbidden action (e.g. index null or negative)");
+            throw new RepositoryException("Unable to insert item: forbidden action (e.g. index null or negative).", e);
         }
+        // only with success, update registry
+        registry.put(item.getIndex(), item);
     }
 
     @Override
     public void update(Item item, int index) throws RepositoryException {
+        if (index != item.getIndex()) {
+            throw new RepositoryException("Unable to update item: given index does not match item contents.");
+        }
+        if (!registry.containsKey(index)) {
+            throw new RepositoryException("Unable to update item: given index is not used.");
+        };
         try {
             dao.updateItem(item);
         } catch (DaoException e) {
-            throw new RepositoryException("Unable to update item: given index does not match");
+            throw new RepositoryException("Unable to update item: given index is not used.", e);
         }
+        // only with success, update registry
+        registry.put(item.getIndex(), item);
     }
 
     @Override
@@ -50,8 +68,10 @@ public class RepositoryImpl implements Repository {
         try {
             dao.deleteItemAtIndex(index);
         } catch (DaoException e) {
-            throw new RepositoryException("Unable to delete item: unable to find item at given index");
+            throw new RepositoryException("Unable to delete item: given index is not used.", e);
         } 
+        // only with success, update registry
+        registry.remove(index);
     }
 
 }
