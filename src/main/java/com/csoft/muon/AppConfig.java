313561c810035fc504e493ddcb4d33358fffcfce
Carlo Morelli <carlomorelli@gmail.com>
2017-08-19 12:38:00 +0200

    Modify DataSource binding to depend on config parameter

--- src/main/java/com/csoft/muon/AppConfig.java
+++ src/main/java/com/csoft/muon/AppConfig.java
@@ -1,11 +1,18 @@
 package com.csoft.muon;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import com.csoft.muon.repository.Repository;
 import com.csoft.muon.repository.RepositoryImpl;
 import com.csoft.muon.repository.datasource.DataSourceFactory;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import com.google.inject.name.Named;
+import com.google.inject.name.Names;
 
 /**
  * Runtime application configuration using Google Guice
@@ -15,10 +22,35 @@ import com.google.inject.AbstractModule;
  */
 public class AppConfig extends AbstractModule {
 
+    private static final String CONF_FILE = "app.properties";
+    
+    
+    
     @Override
     protected void configure() {
+        
+        Properties properties = new Properties();
+        try {
+            properties.load(getClass().getClassLoader().getResourceAsStream(CONF_FILE));
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to access configuration file '" + CONF_FILE + "'.", e);
+        }
+        
         bind(Repository.class).to(RepositoryImpl.class);
-        bind(DataSource.class).toInstance(DataSourceFactory.getPostgresHikariCPDataSource());
+        bindConstant().annotatedWith(Names.named("type")).to(properties.getProperty("dataSource.type"));
     }
 
+    
+    
+    @Provides
+    @Singleton
+    public DataSource getDataSource(@Named("type") String type) {
+        if (type.equals("postgres-hikari")) {
+            return DataSourceFactory.getPostgresHikariCPDataSource();
+        } else if (type.equals("postgres-simple")) {
+            return DataSourceFactory.getPosgresSimpleDataSource();
+        } else {
+            return DataSourceFactory.getH2DataSource();
+        }
+    }
 }
